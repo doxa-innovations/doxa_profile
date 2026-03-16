ce31506d8f38c05a66e76b8072e8fff71ea7366d
Tsion Birhanu <tsionbirhanu08@gmail.com>
2026-03-16 06:26:34 -0700

    feat: implement specialize section UI with animated stacked cards

---
+++ app/specialize/types.ts
@@ -0,0 +1,16 @@
+// Specialize feature 
+
+export interface Tab {
+  id:          string;
+  title:       string;
+  description: string;
+}
+
+export interface StarConfig {
+  top:      string;
+  left:     string;
+  size:     number;
+  opacity:  number;
+  duration: number;
+  delay:    number;
+}
